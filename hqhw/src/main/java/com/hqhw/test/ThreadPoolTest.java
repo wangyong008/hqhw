5f56ade6e5787fa089e0a9bba2d39ca67e7cff50
wangyong008 <1151360186@qq.com>
2019-05-07 14:48:57 +0800

    1

---
+++ hqhw/src/main/java/com/hqhw/test/ThreadPoolTest.java
@@ -0,0 +1,67 @@
+package com.hqhw.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.hqhw.other.server.impl.tests;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:applicationContext.xml" })
+public class ThreadPoolTest {
+
+	@Autowired
+	ThreadPoolTaskExecutor threadPoolTask;
+	
+	@Autowired
+    private TaskExecutor executor;
+	
+	@Autowired
+	tests t;
+	
+//	@Test
+//	public void callableTest() {
+//		Future<Integer> future = threadPoolTask.submit(new Callable<Integer>() {
+//
+//			@Override
+//			public Integer call() throws Exception {
+//				int i = 1;
+//				return i;
+//			}
+//		});
+//		
+//		try {
+//			System.out.println(future.get());
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (ExecutionException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+	
+	    
+	    public void filesMng(String path, String fileName) {
+	    	threadPoolTask.execute(new CutFilesThread(path,fileName));
+	    }
+	    
+	    private class CutFilesThread implements Runnable {
+	        private String path;
+	        private String fileName;
+	        private CutFilesThread(String path, String fileName) {
+	            super();
+	            this.path = path;
+	            this.fileName = fileName;
+	        }
+	        @Override
+	        public void run() {
+	            System.out.println("barry... run...");
+	        }
+	    }
+    
+}
