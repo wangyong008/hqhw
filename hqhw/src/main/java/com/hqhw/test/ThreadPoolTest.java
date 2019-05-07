5681fa6dda133f6033ad14db15ada64c5a50ed3c
wangyong008 <1151360186@qq.com>
2019-05-07 14:58:42 +0800

    2

--- hqhw/src/main/java/com/hqhw/test/ThreadPoolTest.java
+++ hqhw/src/main/java/com/hqhw/test/ThreadPoolTest.java
@@ -1,6 +1,5 @@
 package com.hqhw.test;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.task.TaskExecutor;
